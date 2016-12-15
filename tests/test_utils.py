b803a475b12d83b53beba6b492128368f7f0d1f8
Marvin Steadfast <marvin@xsteadfastx.org>
2016-12-15 13:51:01 +0100

    tests for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+
+from mock import Mock
+
+
+from mopidy_emby import utils
+
+
+def test_decorator():
+    func = Mock()
+    decorated_func = utils.cache()
+    decorated_func(func)
+    func()
+
+    assert func.called is True
+    assert decorated_func._call_count == 1
+
+
+def test_set_default_cache():
+
+    @utils.cache()
+    def returnstring():
+        return 'ok'
+
+    assert returnstring() == 'ok'
+
+
+def test_set_ttl_cache():
+    func = Mock()
+    decorated_func = utils.cache(func, ttl=5)
+    func()
+
+    assert func.called is True
+    assert decorated_func._call_count == 1
+    assert decorated_func.ttl == 5
