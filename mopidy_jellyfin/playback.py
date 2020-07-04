5d8bc334618f9595f646c0486788f0b2f1ad396b
mcarlton00 <mcarlton00@gmail.com>
2020-07-03 20:34:15 -0400

    Add max bitrate option to enable transcoding

--- mopidy_jellyfin/playback.py
+++ mopidy_jellyfin/playback.py
@@ -24,9 +24,12 @@ class JellyfinPlaybackProvider(backend.PlaybackProvider):
             session_id = ''.join(random.choice(letters) for _ in range(20))
             # List of supported containers
             container = 'opus,mp3,aac,m4a,flac,webma,webm,wav,ogg,mpa,wma'
+            bitrate = self.backend.remote.max_bitrate
+            if not bitrate:
+                bitrate = '140000000'
 
             url_params = {
-                'MaxStreamingBitrate': '140000000',
+                'MaxStreamingBitrate': bitrate,
                 'api_key': self.backend.remote.token,
                 'UserId': self.backend.remote.user_id,
                 'DeviceId': mopidy_jellyfin.Extension.device_id,
